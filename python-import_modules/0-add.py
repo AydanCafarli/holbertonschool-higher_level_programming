0aa7c397c396592fc3ce7a137adcc3a96f7b65c4
SəAydanCafarli <saydancfrli478@gmail.com>
2026-04-18 19:09:19 +0000

    Fix: remove extra add_0 occurrence and fix style

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-"""
-0-add.py - Imports a simple function from a simple file.
-This script assigns values to variables a and b, then prints
-the result of adding them using a function from add_0.py.
-"""
-
-
 if __name__ == "__main__":
     from add_0 import add
 
