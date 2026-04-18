e8a78308b4e2a13604d4cfa3debbcc213b240c27
SəAydanCafarli <saydancfrli478@gmail.com>
2026-04-18 14:24:54 +0000

    Clean code

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
-
 def uppercase(str):
-    """Sətirdəki bütün kiçik hərfləri böyük hərflə çap edən funksiya."""
     for char in str:
-        # Simvolun ASCII kodunu alırıq
-        code = ord(char)
-        # Əgər simvol 'a' (97) və 'z' (122) arasındadırsa (yəni kiçik hərfdirsə)
-        if code >= 97 and code <= 122:
-            # Ondan 32 çıxıb böyük hərfi tapırıq
-            char = chr(code - 32)
-        
-        # Simvolu çap edirik (end="" istifadə edirik ki, yan-yana düzülsün)
+        if ord(char) >= 97 and ord(char) <= 122:
+            char = chr(ord(char) - 32)
         print("{}".format(char), end="")
-    
-    # Bütün sətir bitdikdən sonra yeni sətirə keçmək üçün bir boş print
     print("")
