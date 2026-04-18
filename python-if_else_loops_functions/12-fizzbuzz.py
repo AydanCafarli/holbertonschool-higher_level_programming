0429d1b9047b6b02b7f6c3af164539b1ee305433
SəAydanCafarli <saydancfrli478@gmail.com>
2026-04-18 14:41:00 +0000

    Add FizzBuzz function

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    """Prints numbers 1 to 100 with FizzBuzz logic."""
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz", end="")
+        elif i % 3 == 0:
+            print("Fizz", end="")
+        elif i % 5 == 0:
+            print("Buzz", end="")
+        else:
+            print("{}".format(i), end="")
+        
+        # Hər elementdən sonra (sonuncudan başqa) boşluq qoyuruq
+        if i < 100:
+            print(" ", end="")
