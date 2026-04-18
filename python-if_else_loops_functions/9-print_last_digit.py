a21d4dfe1f0b40164bd32c3f9af9fbc2095d8c4a
SəAydanCafarli <saydancfrli478@gmail.com>
2026-04-18 14:32:30 +0000

    Final cleanup for pycodestyle

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-
 def print_last_digit(number):
-    """Ədədin sonuncu rəqəmini çap edir və qaytarır."""
-    # Ədədin mənfi olub-olmadığını yoxlayırıq və son rəqəmi tapırıq
+    """Prints and returns the last digit of a number."""
     if number < 0:
         last_digit = (number * -1) % 10
     else:
         last_digit = number % 10
-    
-    # Rəqəmi çap edirik (end="" istifadə etmirik, çünki main-də yan-yana istənilir)
+
     print("{}".format(last_digit), end="")
-    
-    # Tapdığımız rəqəmi funksiyadan geri qaytarırıq (return)
     return last_digit
