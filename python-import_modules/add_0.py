73f2e3660adf1883d812e7db9085e8b4f13b57fd
SəAydanCafarli <saydancfrli478@gmail.com>
2026-04-18 18:46:21 +0000

    0-add.py tapşırığı hazırlandı

---
+++ python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Mənim toplama funksiyam
+    Arqumentlər:
+        a: birinci tam ədəd
+        b: ikinci tam ədəd
+    Qaytarır:
+        Toplama nəticəsi. a + b
+    """
+    return (a + b)
