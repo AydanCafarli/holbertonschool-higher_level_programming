29174c66b862db5aff54689294157d6b7c0359ed
SəAydanCafarli <saydancfrli478@gmail.com>
2026-04-18 14:43:05 +0000

    Fix FizzBuzz spacing and trailing space

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    """Prints numbers 1 to 100 with FizzBuzz logic."""
+    """Prints numbers 1 to 100 separated by a space."""
     for i in range(1, 101):
         if i % 15 == 0:
-            print("FizzBuzz", end="")
+            print("FizzBuzz", end=" ")
         elif i % 3 == 0:
-            print("Fizz", end="")
+            print("Fizz", end=" ")
         elif i % 5 == 0:
-            print("Buzz", end="")
+            print("Buzz", end=" ")
         else:
-            print("{}".format(i), end="")
-        
-        # Hər elementdən sonra (sonuncudan başqa) boşluq qoyuruq
-        if i < 100:
-            print(" ", end="")
+            print("{}".format(i), end=" ")
