dbdf411b5ce877dd955dfddeb4235139f1493187
AydanCafarli <aydancfrli478@gmail.com>
2026-04-19 08:52:06 +0000

    Fix file location and add README

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # Yeni bir matris yaradırıq
-    # Hər bir 'row' (sətir) üçün yeni list yaradılır və içindəki elementlər kvadrat yüksəldilir
     return [[x**2 for x in row] for row in matrix]
