7551d51261852e094170b62590c71d05b27b43d0
AydanCafarli <aydancfrli478@gmail.com>
2026-04-20 16:41:41 +0000

    Fix W293 and implement roman_to_int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,7 @@ def roman_to_int(roman_string):
         'I': 1, 'V': 5, 'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000
     }
-    
+
     res = 0
     for i in range(len(roman_string)):
         current_val = roman_dict.get(roman_string[i], 0)
