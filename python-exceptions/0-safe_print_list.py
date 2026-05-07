92a4f1f2894236abc7623fdd47aa880faaeb92aa
AydanCafarli <aydancfrli478@gmail.com>
2026-05-07 16:03:08 +0000

    Add safe_print_list function

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
@@ -8,6 +7,5 @@ def safe_print_list(my_list=[], x=0):
             count += 1
         except IndexError:
             break
-    print("")  # Elementlərdən sonra yeni sətir
+    print("")
     return count
-
