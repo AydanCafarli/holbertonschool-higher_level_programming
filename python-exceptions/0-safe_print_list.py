7919636a9c2bf6d702fd04f956ea636e76c697a2
AydanCafarli <aydan.cafarli@gmail.com>
2026-06-06 18:30:44 +0000

    Fix: Move files to python-exceptions directory

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    """Prints x elements of a list."""
     count = 0
     for i in range(x):
         try:
@@ -8,5 +8,5 @@ def safe_print_list(my_list=[], x=0):
             count += 1
         except IndexError:
             break
-    print("")
+    print("")  # Elementlərdən sonra yeni sətir
     return count
